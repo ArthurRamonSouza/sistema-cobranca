f2dc3555280cf320df1be69146ac559f2ba1cfad
Arthur Ramon Souza <arthuramon.souza93@hotmail.com>
2023-02-06 14:00:29 -0300

    Rename class

--- src/main/java/com/haterteam/sistemacobranca/dto/AlunoDTO.java
+++ src/main/java/com/haterteam/sistemacobranca/dto/AlunoDTO.java
@@ -4,7 +4,7 @@ import io.micrometer.common.lang.Nullable;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
-public class RequisicaoNovoAluno {
+public class AlunoDTO {
 
     @NotBlank
     private String cpf;
