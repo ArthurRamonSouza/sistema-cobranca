8f917802a08da1546e84859dd0ff05b40d37a425
Arthur Ramon Souza <arthuramon.souza93@hotmail.com>
2023-02-06 13:44:00 -0300

    Create a DTO to a new Aluno request

---
+++ src/main/java/com/haterteam/sistemacobranca/dto/RequisicaoNovoAluno.java
@@ -0,0 +1,76 @@
+package com.haterteam.sistemacobranca.dto;
+
+import io.micrometer.common.lang.Nullable;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class RequisicaoNovoAluno {
+
+    @NotBlank
+    private String cpf;
+
+    @NotBlank
+    private String nome;
+
+    @NotBlank
+    private int idade;
+
+    @Nullable
+    private String email;
+
+    @NotBlank
+    private int celular;
+
+    @NotBlank
+    @Size(max = 8)
+    private String cep;
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getCelular() {
+        return celular;
+    }
+
+    public void setCelular(int celular) {
+        this.celular = celular;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+}
