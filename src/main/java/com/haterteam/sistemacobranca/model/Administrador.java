f6c774f208f8e1a2ddb39c0c58b9cb7fcb071301
Arthur Ramon Souza <arthuramon.souza93@hotmail.com>
2023-02-06 07:15:15 -0300

    Create new class

---
+++ src/main/java/com/haterteam/sistemacobranca/model/Administrador.java
@@ -0,0 +1,26 @@
+package com.haterteam.sistemacobranca.model;
+
+public class Administrador {
+
+    private String nome;
+    private String senha;    
+
+    public Administrador(String nome, String senha) {
+        this.nome = nome;
+        this.senha = senha;
+    }
+    
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getSenha() {
+        return senha;
+    }
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }    
+
+}
