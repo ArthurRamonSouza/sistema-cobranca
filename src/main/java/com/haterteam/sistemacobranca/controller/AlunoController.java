2d1877e4c49336b3a6adbc462041262fc7a8afea
Arthur Ramon Souza <arthuramon.souza93@hotmail.com>
2023-02-07 15:07:07 -0300

    Method get test

--- src/main/java/com/haterteam/sistemacobranca/controller/AlunoController.java
+++ src/main/java/com/haterteam/sistemacobranca/controller/AlunoController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,6 +25,11 @@ public class AlunoController {
 
     public AlunoController(AlunoService alunoService){
         this.alunoService = alunoService;
+    }
+
+    @GetMapping("")
+	public String index(){
+        return "Aluno Home";
     }
 
     @PostMapping
